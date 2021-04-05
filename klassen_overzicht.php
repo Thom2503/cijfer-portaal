cc638805f17a8fd963e3829f2193e6f486228f13
Thom2503 <thomveldhuis03@gmail.com>
2021-04-05 16:07:25 +0200

    small changes

--- klassen_overzicht.php
+++ klassen_overzicht.php
@@ -63,7 +63,7 @@
      {
        ?>
        <div class="tab">
-         <button class="tablinks" onclick="openCity(event, '<?php echo $rs['VakNaam'] ?>')"><?php echo $rs['VakNaam'] ?></button>
+         <button class="tablinks" onclick="openTab(event, '<?php echo $rs['VakNaam'] ?>')"><?php echo $rs['VakNaam'] ?></button>
        </div>
        <?php
        $klas = "SELECT DISTINCT s.StudentID, s.Voornaam, s.Achternaam, s.Klas_ID, vk.KlasID
@@ -101,7 +101,7 @@
 }
 ?>
  <script>
- function openCity(evt, cityName) {
+ function openTab(evt, tabName) {
    var i, tabcontent, tablinks;
    tabcontent = document.getElementsByClassName("tabcontent");
    for (i = 0; i < tabcontent.length; i++) {
@@ -111,7 +111,7 @@
    for (i = 0; i < tablinks.length; i++) {
      tablinks[i].className = tablinks[i].className.replace(" active", "");
    }
-   document.getElementById(cityName).style.display = "block";
+   document.getElementById(tabName).style.display = "block";
    evt.currentTarget.className += " active";
  }
  </script>
