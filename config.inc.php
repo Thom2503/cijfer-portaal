f39cf81822ebb144ae589cb2a1b391c5e25353bd
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-05-19 09:40:00 +0200

    lllklklklk

--- config.inc.php
+++ config.inc.php
@@ -12,6 +12,8 @@ $db_username = 'root';
 $db_password = '';
 $db_database = 'cijfer_portaal';
 
+error_reporting(E_ERROR | E_PARSE );
+
 //database connectie
 $mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
